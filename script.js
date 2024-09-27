ce66150a15d104f47b1b5fc916753b8f17533bfb
H Joseph <hjoseph777@gmail.com>
2024-09-27 16:46:14 -0400

    main

---
+++ script.js
@@ -0,0 +1,74 @@
+//Step 2: If-else Condition
+let number = 11; // Assign any number here
+ if (number > 0) {
+    // Output for positive numbers
+    console.log("Positive");
+} else if (number < 0) {
+    // Output for negative numbers
+    console.log("Negative");
+} else {
+    // Output for zero
+    console.log("Zero");
+}
+
+// Switch case
+let day = 3;
+switch (day) {
+    case 1:
+        // Output Sunday
+        console.log("Sunday");
+        break;
+        case 2:
+        // Output Monday
+        console.log("Monday");
+        break;
+        case 3:
+        // Output Tuesday
+        console.log("Tuesday");
+        break;
+        case 4:
+        // Output Wednesday
+        console.log("Wednesday");
+        break;
+        case 5:
+        // Output Thursday
+        console.log("Thursday");
+        break;
+        case 6:
+        // Output Friday
+        console.log("Friday");
+        break;
+        case 7:
+        // Output Saturday
+        console.log("Saturday");
+        break;
+    // end cases
+    default:
+        // Output Invalid day
+        console.log("Invalid day");     
+    
+    } 
+
+    // for loop
+    for (let i = 0; i < 5; i++) {
+        // Print i
+        console.log(i);
+    }
+
+    // while loop
+    
+     while (i <= 5) {
+        // Print i
+        console.log(i);
+        i++;
+    }
+
+   //Do...While Loop
+   let j = 1; 
+ do {
+ // Print j j++; 
+ console.log(j);
+ j++;
+﻿} while (j <= 5);
+
+
